201fecb3fc47caeca8bcd01e267fdba376a17932
henrythomaz <henrythcampos@gmail.com>
2026-01-23 11:51:51 -0300

    feat: Add routes.js

--- 02-backend/nodejs/2-frameworks/express/exercises/ex010/src/routes/routes.js
+++ 02-backend/nodejs/2-frameworks/express/exercises/ex010/src/routes/routes.js
@@ -1,8 +1,28 @@
 import { Router } from "express";
-import message from "../app/controllers/HelloController.js";
+import customers from "../app/controllers/CustomersController.js";
+import contacts from "../app/controllers/ContactsController.js";
+import users from "../app/controllers/UsersController.js";
 
 const routes = new Router();
 
-routes.get("/hello", message.hello);
+// Rota dos Clientes
+routes.get("/customers", customers.index);
+routes.get("/customers/:id", customers.show);
+routes.post("/customers", customers.create);
+routes.put("/customers/:id", customers.update);
+routes.delete("/customers/:id", customers.destroy);
+
+// Rota dos Contatos 
+routes.get("/customers/:customerId/contacts", contacts.index);
+routes.get("/customers/:customerId/contacts/:id", contacts.show);
+routes.post("/customers/:customerId/contacts", contacts.create);
+routes.put("/customers/:customerId/contacts/:id", contacts.update);
+routes.delete("/customers/:customerId/contacts/:id", contacts.destroy);
+
+routes.get("/users", users.index);
+routes.get("/users/:id", users.show);
+routes.post("/users", users.create);
+routes.put("/users/:id", users.update);
+routes.delete("/users/:id", users.destroy);
 
 export default routes;
