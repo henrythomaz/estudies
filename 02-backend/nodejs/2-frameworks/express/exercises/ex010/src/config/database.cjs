a1defe3f4d380c3a0b124fa1f0f3ba4dbbc01e9a
henrythomaz <henrythcampos@gmail.com>
2026-01-23 11:52:26 -0300

    feat: Add database config

--- 02-backend/nodejs/2-frameworks/express/exercises/ex010/src/config/database.cjs
+++ 02-backend/nodejs/2-frameworks/express/exercises/ex010/src/config/database.cjs
@@ -1,16 +1,23 @@
-require("dotenv").config();
+const path = require("path");
+const dotenv = require("dotenv");
+
+const envFile = process.env.DOCKER === "true" ? ".env" : ".env.local";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), envFile),
+});
 
 module.exports = {
   dialect: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 
   define: {
-    timestamp: true,
+    timestamps: true,
     underscored: true,
     underscoredAll: true,
-  }
-}
+  },
+};
