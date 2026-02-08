32322becb21b6589aa11ee6f8389193cba862d22
henrythomaz <henrythcampos@gmail.com>
2026-02-08 22:48:37 +0000

    feat(python): Add ex003

---
+++ 08-machine-learn/1-python/exercises/ex004/tuples.py
@@ -0,0 +1,14 @@
+data = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 7.5)
+
+print(data)
+
+len_data = len(data)
+min_data = min(data)
+max_data = max(data)
+sum_data = sum(data)
+sorted_data = sorted(data)
+
+print(len_data)
+print(min_data)
+print(max_data)
+print(sorted_data)
